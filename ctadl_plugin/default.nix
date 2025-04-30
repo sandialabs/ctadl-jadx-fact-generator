757ecae3d57cb0e23deb80f1c059a473afde3378
Denis Bueno <denbuen@sandia.gov>
2025-04-30 17:25:10 -0600

    Publishing tweaks

--- ctadl_plugin/default.nix
+++ ctadl_plugin/default.nix
@@ -5,21 +5,32 @@
   makeWrapper,
   lib,
 }:
-python3.pkgs.buildPythonPackage rec {
-  pname = "ctadl-jadx-fact-generator-plugin";
-  version = lib.strings.removeSuffix "\n" (builtins.readFile ./src/ctadl_jadx_fact_generator_plugin/VERSION);
-  src = ./.;
-
-  doCheck = false;
-
-  buildInputs = [];
-  propagatedBuildInputs = [jadxFactgen];
-
-  postConfigure = ''
-    cp ${jadxFactgen}/lib/*.jar src/ctadl_jadx_fact_generator_plugin/
-  '';
-
-  passthru = {
-    makeWrapperArgs = [''--set JAVA_HOME ${jre.home}''];
+let
+  withPythonWheel = final: prev: {
+    outputs = prev.outputs ++ ["whl"];
+    postInstall = prev.postInstall or ""
+    + ''
+      mkdir $whl
+      cp -r dist/* $whl/
+    '';
   };
-}
+  pkg = python3.pkgs.buildPythonPackage rec {
+    pname = "ctadl-jadx-fact-generator-plugin";
+    version = lib.strings.removeSuffix "\n" (builtins.readFile ./src/ctadl_jadx_fact_generator_plugin/VERSION);
+    src = ./.;
+
+    doCheck = false;
+
+    buildInputs = [];
+    propagatedBuildInputs = [jadxFactgen];
+
+    postConfigure = ''
+      cp ${jadxFactgen}/lib/*.jar src/ctadl_jadx_fact_generator_plugin/
+    '';
+
+    passthru = {
+      makeWrapperArgs = [''--set JAVA_HOME ${jre.home}''];
+    };
+  };
+in
+  pkg.overrideAttrs withPythonWheel
